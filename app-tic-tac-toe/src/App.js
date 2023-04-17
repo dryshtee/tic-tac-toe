5fa28000f2472bbb573d81c5d9f45cd17ce5ed55
Woozir Manilall <dryshtee.woozir@accenture.com>
2023-04-17 11:12:47 +0400

    create reusable component to avois messy, duplicated code using react's component architecture

--- app-tic-tac-toe/src/App.js
+++ app-tic-tac-toe/src/App.js
@@ -5,32 +5,37 @@ import './App.css';
  * export JavaScript keyword: make function accessible outside this file
  * default keyword tells other files using your code taht it's the main function    
  */
+
+function Square({ value }) {
+  return <button className="square">{ value }</button>;
+}
+
 export default function Board() {
-/**
+  /**
  * returns (whatever comes after is returned as a value to the caller of the function) 
  * a button (JSX element: combination of JS code + HTML tags)
  * className: button property or prop that tells CSS how to style the button
  * X: text displayed inside the button
- */  
+ */ 
   return (
     <>
-    <div className="board-row">
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-    </div>
-    <div className="board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
-    </div>
-    <div className="board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
-    </div>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
     </>
   );
 }
 
-// export default App;
+/* export default App; */
